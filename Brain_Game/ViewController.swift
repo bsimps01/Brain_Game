0ca4cbecc4342cb7ceb85228f187f01632732788
Ben Simpson <b.albertsimpson@gmail.com>
2019-12-09 23:37:20 -0800

    Initial Commit

---
+++ Brain_Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brain_Game
+//
+//  Created by Benjamin Simpson on 12/9/19.
+//  Copyright © 2019 Benjamin Simpson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

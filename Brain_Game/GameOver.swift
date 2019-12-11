2372e49cd3e676abaa6f2022d074259066529d16
Ben Simpson <b.albertsimpson@gmail.com>
2019-12-11 04:21:08 -0800

    updates in progress

--- Brain_Game/GameOver.swift
+++ Brain_Game/GameOver.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2019 Benjamin Simpson. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class gameOverController: UIViewController {
+    
+    @IBOutlet var sceneView: UIView!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    var highScore: Int = 0
+    var message: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupScene()
+}
+
+    func setupScene() {
+        playAgainButton.layer.masksToBounds = true
+        playAgainButton.layer.cornerRadius = 20
+    
+    scoreLabel.text = String(highScore)+" points"
+}
+}
--- Brain_Game/ViewController.swift
+++ Brain_Game/ViewController.swift
@@ -10,11 +10,153 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var sceneView: UIView!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var noButton: UIButton!
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var meaningLabel: UILabel!
+    
+    @IBOutlet weak var colortextLabel: UILabel!
+    var highScore: Int = 0
+    var score: Int = 0 {
+        didSet {
+            self.messageLabel.font = UIFont.systemFont(ofSize: 50)
+    }
+    }
+    var timerCount: Int = 5
+    var message: String = "Game Over"
+    
+    let colors: [String: UIColor] = ["red": .red,
+    "green": .green,
+    "yellow": .yellow,
+    "purple": .purple,
+    "orange": .orange,
+    "blue": .blue,
+    "black": .black,
+    "gray": .gray,
+    "white": .white,
+    ]
+    @IBAction func yesButtonAction(_ sender: Any) {
+        if(colors[currentText] == currentColor){
+            self.check(winOrLose: true)
+        }
+        else {
+            self.check(winOrLose: false)
+            }
+            refreshGame()
+        }
+    var currentColor: UIColor = UIColor.white
+    var currentText: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        refreshGame()
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showEndGame" {
+            if let gameOverController = segue.destination as? gameOverController {
+                    gameOverController.highScore = self.highScore
+                    gameOverController.message = self.message
     }
 
+    func setupScene() {
+        meaningLabel.layer.masksToBounds = true
+        colortextLabel.layer.masksToBounds = true
+        meaningLabel.layer.cornerRadius = 20
+        colortextLabel.layer.cornerRadius = 20
+        yesButton.layer.cornerRadius = 5
+        noButton.layer.cornerRadius = 5
+        yesButton.backgroundColor = UIColor.init(displayP3Red: 30, green: 30, blue: 30, alpha: 0.4)
+        noButton.backgroundColor = UIColor.init(displayP3Red: 30, green: 30, blue: 30, alpha: 0.4)
+        scoreLabel.backgroundColor = UIColor.init(displayP3Red: 0, green: 0, blue: 0, alpha: 0.2)
+        scoreLabel.textColor = UIColor(white: 1, alpha: 0.5)
+        timerLabel.backgroundColor = UIColor.init(displayP3Red: 0, green: 0, blue: 0, alpha: 0.2)
+        timerLabel.textColor = UIColor(white: 1, alpha: 0.5)
+}
 
+    func runTimer() {
+        _ = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+        self.timerCount -= 1
+        self.timerLabel.text = "Time  0:0"+String(self.timerCount)+""
+        
+        if self.timerCount == 0 {
+            self.timerCount = 0
+            timer.invalidate()
+            self.message = "Time is Up!"
+            self.performSegue(withIdentifier: "showEndGame", sender: self)
+            
+        }
+        if self.timerCount < 5 {
+            self.messageLabel.text = ""
+            self.messageLabel.font = UIFont.systemFont(ofSize: 35)
+            self.animate(time: "0:0"+String(self.timerCount))
+            self.messageLabel.textColor = self.colors.randomElement()?.value
+        }
+    }
+}
+
+    func animate(time: String, color:UIColor = .white) {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: {
+        self.messageLabel.alpha = 0.0
+        }, completion: {
+            (finished: Bool) -> Void in
+
+            if ((color == .green) || (color == .red)){
+                self.messageLabel.textColor = color
+            }
+            self.messageLabel.text = time
+
+            // Fade in
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+                self.messageLabel.alpha = 1.0
+                }, completion: nil)
+    })
+}
+
+func refreshGame(){
+    currentText = colors.randomElement()!.key
+    currentColor = colors.randomElement()!.value
+    let randomColor = colors.randomElement()?.key
+    
+    meaningLabel.text = randomColor
+    colortextLabel.text = currentText
+    meaningLabel.textColor = currentColor
+    
+    
+    if (score <= -1) {
+        score = 0
+        scoreLabel.text = "HighScore   "+String(highScore)+""
+        self.timerCount = 0
+        performSegue(withIdentifier: "showEndGame", sender: self)
+        
+    } else {
+        scoreLabel.text = "Score  "+String(score)+""
+    }
+}
+    }
+    
+}
+func check(winOrLose:Bool) {
+        var color:UIColor = .white
+        var messageStr:String = ""
+        
+        if (winOrLose == true) {
+            self.score += 10
+            self.highScore += 10
+            color = .green
+            messageStr = "Correct! +10"
+        }
+        else {
+            self.score -= 20
+            color = .red
+            messageStr = "Wrong! -20"
+        }
+        
+
+        self.animate(time: messageStr, color: color)
+    }
 }
 
